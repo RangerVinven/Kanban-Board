5357ee01283f86a94017eaf1f38302dbb79fafca
Daniel McPherson <daniel.mcpherson@live.co.uk>
2022-06-22 13:19:54 +0100

    Added props to the KanbanItem

--- src/PagesAndComponents/Pages/Kanban/Components/KanbanItem.tsx
+++ src/PagesAndComponents/Pages/Kanban/Components/KanbanItem.tsx
@@ -1,8 +1,12 @@
-export default function KanbanItem() {
+interface Props {
+	text: string
+}
+
+export default function KanbanItem(props: Props) {
   return (
     <div className="w-full">
         <hr />
-        <h3 className="my-1">Make the kanban board</h3>
+        <h3 className="my-1">{props.text}</h3>
     </div>
   )
 }
--- src/PagesAndComponents/Pages/Kanban/Components/KanbanSection.tsx
+++ src/PagesAndComponents/Pages/Kanban/Components/KanbanSection.tsx
@@ -1,12 +1,13 @@
 import KanbanItem from "./KanbanItem";
 
 export default function KanbanSection() {
-  return (
-    <div className="h-3/4 w-64 p-1 bg-white rounded-md">
-		<h1 className="text-xl">Todo</h1>
-		<KanbanItem />
-		<KanbanItem />
-		<hr />
-    </div>
-  )
+
+	return (
+		<div className="h-3/4 w-64 p-1 bg-white rounded-md">
+			<h1 className="text-xl">Todo</h1>
+			<KanbanItem text="Make the kanban board" />
+			<KanbanItem text="Get a slush" />
+			<hr />
+		</div>
+	)
 }
